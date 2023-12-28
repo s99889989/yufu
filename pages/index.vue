80868de2f71d6c4fb640a598540063b8296f5523
BuildTools <unconfigured@null.spigotmc.org>
2023-12-28 17:41:12 +0800

    2023/12/28

---
+++ pages/index.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+// import { Dropdown } from "flowbite-react";
+</script>
+
+<template>
+
+<!--  <div class="container-fluid">-->
+<!--    <h1 class="text-6xl dark:text-white font-semibold text-sky-400">2023</h1>-->
+<!--    <p class="mt-4 text-9xl dark:text-white font-bold text-gray-600">FancyWeb</p>-->
+<!--  </div>-->
+  <div class="bg-success-subtle pt-5">
+    <div class="container text-center bg-white">
+
+      <div class="row">
+        <div class="col-4">
+          <img class="w-100" src="/Logo.jpg" alt="">
+        </div>
+        <div class="col-8 d-flex justify-content-center align-items-center">
+          <p style="font-size: 5rem" class="text-success align-middle" >愈富環保企業社</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-3 p-3">
+          <div class="card" style="width: 18rem;">
+            <img src="https://picsum.photos/200/200?random=1" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">證照1</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3 p-3">
+          <div class="card " style="width: 18rem;">
+            <img src="https://picsum.photos/200/200?random=2" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">證照2</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <img src="https://picsum.photos/400/200?random=3" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=4" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=5" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=6" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=7" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <img src="https://picsum.photos/400/200?random=8" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=4" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=5" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=6" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="https://picsum.photos/200/200?random=7" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-3">
+          <img src="/LineCode.png" class="w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="/LineCode.png" class="w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="/LineCode.png" class="w-100" alt="">
+        </div>
+        <div class="col-3">
+          <img src="/LineCode.png" class="w-100" alt="">
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
