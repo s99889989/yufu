64827d76384505481c68e7e40e2fa5b3c0bf0d66
BuildTools <unconfigured@null.spigotmc.org>
2023-12-28 18:19:06 +0800

    2023/12/28

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
             <img src="https://picsum.photos/200/200?random=1" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">證照1</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
             </div>
           </div>
         </div>
@@ -41,7 +41,7 @@
             <img src="https://picsum.photos/200/200?random=2" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">證照2</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
             </div>
           </div>
         </div>
@@ -49,37 +49,37 @@
 
       <div class="row">
         <div class="col-12">
-          <img src="https://picsum.photos/400/200?random=3" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/水刀_2.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
         <div class="col-3">
-          <img src="https://picsum.photos/200/200?random=4" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/水刀_1.jpg" style="border-radius: 30px;object-fit: cover;" class="p-3 w-100" alt="">
         </div>
-        <div class="col-3">
-          <img src="https://picsum.photos/200/200?random=5" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        <div class="col-3 d-flex justify-content-center align-items-center">
+          <img src="/水刀_2.jpg" style="border-radius: 30px;object-fit: cover;" class="p-3 w-100" alt="">
         </div>
-        <div class="col-3">
+        <div class="col-3 d-flex justify-content-center align-items-center">
           <img src="https://picsum.photos/200/200?random=6" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
-        <div class="col-3">
+        <div class="col-3 d-flex justify-content-center align-items-center">
           <img src="https://picsum.photos/200/200?random=7" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
       </div>
 
       <div class="row">
         <div class="col-12">
-          <img src="https://picsum.photos/400/200?random=8" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_1.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
         <div class="col-3">
-          <img src="https://picsum.photos/200/200?random=4" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_1.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
         <div class="col-3">
-          <img src="https://picsum.photos/200/200?random=5" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_2.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
         <div class="col-3">
-          <img src="https://picsum.photos/200/200?random=6" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_3.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
         <div class="col-3">
-          <img src="https://picsum.photos/200/200?random=7" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_4.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
         </div>
       </div>
 
