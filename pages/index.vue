741bf983fe7ba4e83346b51b65c315f542f5240c
s99889989 <s02048898067@gmail.com>
2024-01-23 18:54:00 +0800

    2023/1/23

--- pages/index.vue
+++ pages/index.vue
@@ -12,118 +12,123 @@
     <div class="container text-center bg-white" style="border-radius: 20px;">
 
       <div class="row">
-        <div class="col-12 col-lg-4">
-          <img class="w-100" src="/Logo.jpg" alt="">
+        <div class="col-12 col-lg-3">
         </div>
-        <div class="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+        <div class="col-12 col-lg-1 justify-content-center">
+          <img style="width: 120px; height: 120px" src="/Logo.jpg" alt="">
+        </div>
+        <div class="col-12 col-lg-7 d-flex">
           <p style="font-size: 5rem" class="text-success align-middle" >愈富環保企業社</p>
         </div>
       </div>
+      <div class="row bg-body border-white ">
+        <div class="col-4">
+          <i class="fa-solid fa-glass-water-droplet"></i>
+          <p class="font-sm">抽水肥</p>
+        </div>
+        <div class="col-4">
+          <i class="fa-solid fa-glass-water-droplet"></i>
+          <p class="">水刀通管</p>
+        </div>
+        <div class="col-4">
+          <i class="fa-solid fa-glass-water-droplet"></i>
+          <p class="">化糞池菌種馴養</p>
+        </div>
 
-
-      <SepticTankBacteria/>
-      <div class="row">
-
-        <div class="col-3">
-          <img src="/septic_tank_bacteria/LINE_ALBUM_化糞池菌相_240122_11.jpg" style="border-radius: 30px;object-fit: cover;" class="p-3 w-100" alt="">
-        </div>
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <img src="/septic_tank_bacteria/LINE_ALBUM_化糞池菌相_240122_14.jpg" style="border-radius: 30px;object-fit: cover;" class="p-3 w-100" alt="">
-        </div>
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <img src="/septic_tank_bacteria/LINE_ALBUM_化糞池菌相_240122_46.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
-        </div>
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <img src="/septic_tank_bacteria/LINE_ALBUM_化糞池菌相_240122_49.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
-        </div>
       </div>
-<!--      <div class="row">-->
-<!--        <div class="col-3 p-3">-->
-<!--          <div class="card" style="width: 18rem;">-->
-<!--            <img src="https://picsum.photos/200/200?random=1" class="card-img-top" alt="...">-->
-<!--            <div class="card-body">-->
-<!--              <h5 class="card-title">證照1</h5>-->
-<!--&lt;!&ndash;              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>&ndash;&gt;-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        <div class="col-3">-->
-
-<!--        </div>-->
-<!--        <div class="col-3">-->
-
-<!--        </div>-->
-<!--        <div class="col-3 p-3">-->
-<!--          <div class="card " style="width: 18rem;">-->
-<!--            <img src="https://picsum.photos/200/200?random=2" class="card-img-top" alt="...">-->
-<!--            <div class="card-body">-->
-<!--              <h5 class="card-title">證照2</h5>-->
-<!--&lt;!&ndash;              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>&ndash;&gt;-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-      <div class="row">
-        <div class="col-12 col-lg-4">
-          <div class="embed-responsive embed-responsive-16by9">
-            <video class="embed-responsive-item w-100 rounded" controls>
-              <source src="/video/727605640.571123.mp4" type="video/mp4">
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        </div>
-        <div class="col-12 col-lg-4">
-          <div class="embed-responsive embed-responsive-16by9 w-100">
-            <video class="embed-responsive-item w-100 rounded" controls>
-              <source src="/video/727605640.740348.mp4" type="video/mp4">
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        </div>
-        <div class="col-12 col-lg-4">
-          <div class="embed-responsive embed-responsive-16by9 w-100">
-            <video class="embed-responsive-item w-100 rounded" controls>
-              <source src="/video/727605640.888858.mp4" type="video/mp4">
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        </div>
+      <div class="row rounded m-1 border-1 border-body-tertiary">
+        <p style="font-size: 2rem" class="font-sm">合法抽水肥</p>
+        <p class="">聯絡電話 0905-537-307</p>
       </div>
 
       <div class="row">
-        <div class="col-12">
-          <img src="/水刀_2.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
+        <div class="col-6">
+          <img src="/抽水肥_3.jpg" style="border-radius: 10px;object-fit: cover;" class="pt-5 w-75" alt="">
+          <p style="font-size: 1.2rem">甲級廢棄物處理證照</p>
         </div>
         <div class="col-6">
-          <img src="/水刀_1.jpg" style="border-radius: 30px;object-fit: cover;" class="p-3 w-100" alt="">
-        </div>
-        <div class="col-6 d-flex justify-content-center align-items-center">
-          <img src="/水刀_2.jpg" style="border-radius: 30px;object-fit: cover;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_3.jpg" style="border-radius: 10px;object-fit: cover;" class="pt-5 w-75" alt="">
+          <p style="font-size: 1.2rem">公民營事業廢棄物清除許可證</p>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-12">
-          <img src="/抽水肥_1.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
+      <div class="row rounded m-1 border-1 border-body-tertiary">
+        <p style="font-size: 2rem" class="font-sm">合法抽水肥</p>
+      </div>
+
+      <div class="d-flex justify-content-center">
+
+        <div class="col-3">
+
+          <img src="/抽水肥_2.jpg" style="border-radius: 10px;object-fit: cover;" class="pt-2 w-100" alt="">
+          <p style="font-size: 1.2rem">抽化糞池前</p>
+        </div>
+        <div class="col-1 d-flex justify-content-center align-items-center">
+          <i style="font-size: 2rem" class="fa-solid fa-arrow-right"></i>
         </div>
         <div class="col-3">
-          <img src="/抽水肥_1.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_3.jpg" style="border-radius: 10px;object-fit: cover;" class="pt-2 w-100" alt="">
+          <p style="font-size: 1.2rem">抽化糞池後</p>
+        </div>
+        <div class="col-1 d-flex justify-content-center align-items-center">
+          <i style="font-size: 2rem" class="fa-solid fa-arrow-right"></i>
         </div>
         <div class="col-3">
-          <img src="/抽水肥_2.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img src="/抽水肥_3.jpg" style="border-radius: 10px;object-fit: cover;" class="pt-2 w-100" alt="">
+          <p style="font-size: 1.2rem">投放菌種</p>
         </div>
+      </div>
+
+      <div class="row rounded m-1 border-1 border-body-tertiary">
+        <p style="font-size: 1.5rem">傳統通管</p>
+        <p>傳統通管機使用各式接頭將管路內的污染物推出，但並無完全清洗管路，仍會有污染物垢</p>
+        <p>殘留於管壁，且再次堵塞的風險，特別無法處理因油質堵住等管路。</p>
+        <p style="font-size: 1.5rem">水刀通管</p>
+        <p>水刀通管機則是利用高壓水槍搭配合適的噴頭清洗管路，以徹底疏通，恢復管路內至接近新品。</p>
+      </div>
+
+      <div class="row justify-content-center">
+
         <div class="col-3">
-          <img src="/抽水肥_3.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img style="height: 25rem" src="/水刀_1.jpg" class="rounded" alt="">
         </div>
+
         <div class="col-3">
-          <img src="/抽水肥_4.jpg" style="border-radius: 30px;" class="p-3 w-100" alt="">
+          <img style="height: 25rem" src="/水刀_2.jpg" class="w-100 rounded" alt="">
+          <p style="font-size: 1.2rem">清洗後</p>
+        </div>
+
+        <div class="col-3">
+          <video style="height: 25rem" class="embed-responsive-item rounded" controls>
+            <source src="/video/727605640.571123.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+          </video>
+          <p style="font-size: 1.2rem">投放菌種</p>
+        </div>
+      </div>
+
+
+      <div class="flex">
+        <p style="font-size: 1.5rem">與我們聯絡</p>
+        <div>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
+          <i class="px-2 fa-xl fa-solid fa-arrow-down"></i>
         </div>
       </div>
 
       <div class="row">
         <div class="col-12 d-flex flex-column text-start">
-<!--          <p  style="font-size: 2rem" class="ps-3" >地址: XXXXXXXXXXXXXXXXXXXX</p>-->
-<!--          <p  style="font-size: 2rem" class="ps-3" >連略方式: XXXXXXXXXXXXXXXXXXXX</p>-->
+
+          <p  style="font-size: 2rem" class="ps-3" >地址: XXXXXXXXXXXXXXXXXXXX</p>
+          <p  style="font-size: 2rem" class="ps-3" >連略方式: XXXXXXXXXXXXXXXXXXXX</p>
 
         </div>
         <div class="col-3">
